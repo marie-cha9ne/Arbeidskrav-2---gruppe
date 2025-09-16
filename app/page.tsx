c8e7c9ea84ea840c0ce252e39726683393b0edaa
CelesteEvertsen <evertsen.celeste@gmail.com>
2025-09-16 22:29:47 +0200

    teaser er klar. designe er helt OK

--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,94 @@
-
 import styles from "./components/Main.module.css";
-import reactlogo from "./assets/react.svg";
+import reactlogo from "../public/react.svg";
+import galaxy from "../public/galaxy.jpg";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.aboveTheFold}>
-        <div className={styles.reactAboveThefold}>
-          <p className={styles.AboveThefoldHeadline}>React</p>
+      <div className={styles.teaser}>
+        <div className={styles.textbox}>
+          <h1>Velkommen til The Wizards of the Web! </h1>
+          <div className={styles.introtext}>
+            <p>
+              Gjør som tusenvis av andre webutvikling-interesserte: Lær
+              spennenende teknikker, ta kurset og bli en ekte Web-Wizard du og!
+              .
+            </p>
+          </div>
+          <div className={styles.learn}> 
+            <p>Her kan du lese om blant annet:</p>
+            <ul>
+              <li>Designprinsipper</li>
+              <li>Javascript</li>
+              <li>React</li>
+            </ul>
+          </div>
+
+          <div className={styles.test}>
+            <p>
+              <strong>
+                Når du føler deg trygg på teknikkene, utfordrer vi deg til 10
+                oppgaver. <br /> Om du består disse, har du bestått testen og kan kalle
+                deg for en Wizard of the Web. reagerer{" "}
+              </strong>
+            </p>
+          </div>
+           <button className={styles.callToActionTeaser} type="button">
+          Kom i gang!
+        </button>
+        </div>
+       
+      </div>
+      <div className={styles.underTheFold}>
+        <div className={styles.reactunderThefold}>
+          <Image
+            className={styles.reactlogo}
+            src={reactlogo}
+            alt="React logo"
+            width={100}
+            height={100}
+          />
+          <p className={styles.underThefoldHeadline}>React</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos autem
             alias fugiat consectetur dolorem saepe ipsum eligendi enim aliquam,
             reiciendis sed dicta. In ipsa eaque quam, temporibus sapiente error
             sint.
           </p>
-         <Link className={styles.callToAction} href='/learnreact'> Lær React</Link>
+          <Link className={styles.callToAction} href="/subject/sub-react">
+            {" "}
+            Lær React
+          </Link>
         </div>
-        <div className={styles.JavaScriptAboveTheFold}>
-            <p className={styles.AboveThefoldHeadline}>Javascript</p>
-            <p>
+        <div className={styles.JavaScriptunderTheFold}>
+          <p className={styles.underThefoldHeadline}>Javascript</p>
+          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos autem
             alias fugiat consectetur dolorem saepe ipsum eligendi enim aliquam,
             reiciendis sed dicta. In ipsa eaque quam, temporibus sapiente error
             sint.
           </p>
-          <Link className={styles.callToAction} href='/learnjavascript'> Lær Javascript</Link>
-            </div>
-        <div className={styles.cssAboveThefold}>
-            <p className={styles.AboveThefoldHeadline}>CSS</p>
-            <p>
+          <Link className={styles.callToAction} href="/subject/sub-js">
+            {" "}
+            Lær Javascript
+          </Link>
+        </div>
+        <div className={styles.cssunderThefold}>
+          <p className={styles.underThefoldHeadline}>CSS</p>
+          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos autem
             alias fugiat consectetur dolorem saepe ipsum eligendi enim aliquam,
             reiciendis sed dicta. In ipsa eaque quam, temporibus sapiente error
             sint.
           </p>
           <p className={styles.cssLogo}></p>
-          <Link className={styles.callToAction} href='/learncss'> Lær CSS</Link>
-            </div>
-      </div>
-
-      <div className={styles.reactDiv}>
-        <img src={reactlogo} className={styles.logoReact} alt="React logo" />
-        <p>REAGER TIL VERDEN RUNDT DEG</p>
-        <p>
-          Lær React ett rammerverk som gir nettsiden din ett løff verden vil
-          <strong> reagerer </strong> på.
-        </p>
-        <button className={styles.callToAction} type="button">
-          REACT
-        </button>
-      </div>
-      <div className={styles.JavascriptDiv}>
-        <h1>Javascript</h1>
-        <p> VIL DU BLI POPPULÆR?</p>
-        <p> Lær deg det mest poppulære kode sprøket i dag</p>
-        <button className={styles.callToAction} type="button">
-          JavaScript
-        </button>
-      </div>
-      <div className={styles.CSSDiv}>
-        <h1>CSS</h1>
-        <p>LIKER DU PENE TING, MEN OM OGSÅ ER LETT Å BRUKE</p>
-        <p>CSS er veien til ett penere og flottere WEB</p>
-        <button className={styles.callToAction} type="button">
-          CSS
-        </button>
+          <Link className={styles.callToAction} href="/subject/sub-css">
+            {" "}
+            Lær CSS
+          </Link>
+        </div>
       </div>
     </main>
   );
--- app/reactcourse/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function reactcourse(){
-    return(
-        <main>
-            <h1>Velkommen til KURSET</h1>
-        </main>
-    )
-}
